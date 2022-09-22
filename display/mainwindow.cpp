69cec400b02321651c59ef0ecaf4e9043828bb43
zheyi613 <zheyi880613@gmail.com>
2022-09-22 13:58:44 +0800

    update qcustomplot for two charts

--- display/mainwindow.cpp
+++ display/mainwindow.cpp
@@ -17,48 +17,57 @@ MainWindow::MainWindow(QWidget *parent)
     port->setFlowControl(QSerialPort::NoFlowControl);
     MySerialPortReader *reader = new MySerialPortReader(port);
 
-    // Custom Plot setting
+    // Custom plot layout setup
     QCustomPlot *plot = ui->customPlot_attitude;
+    plot->plotLayout()->clear();
+    plot->setFont(font());
+    QCPLegend *legend = new QCPLegend();
+    legend->setFont(QFont("Consolas", 11, QFont::Medium));
+    legend->setVisible(true);
+    legend->setBorderPen(Qt::NoPen);
+    legend->setFillOrder(QCPLayoutGrid::foColumnsFirst);
+    QCPAxisRect *rect1 = new QCPAxisRect(plot);
+    rect1->setupFullAxesBox(true);
+    QCPAxisRect *rect2 = new QCPAxisRect(plot);
+    rect2->setupFullAxesBox(true);
+    plot->plotLayout()->addElement(0, 0, legend);
+    plot->plotLayout()->addElement(1, 0, rect1);
+    plot->plotLayout()->addElement(2, 0, rect2);
+    plot->plotLayout()->setRowStretchFactor(0, 0.001);
+    plot->plotLayout()->setRowStretchFactor(2, 0.5);
 
-    QString legend_name[] = {"roll", "pitch", "yaw"};
+    foreach (QCPAxisRect *rect, plot->axisRects()) {
+        foreach (QCPAxis *axis, rect->axes()) {
+            axis->setLayer("axes");
+            axis->grid()->setLayer("grid");
+        }
+    }
+
+    plot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom);
+
+    // Custom plot graph setup
+    QString name[] = {"roll", "pitch", "yaw", "temperature"};
 
     for (int i = 0; i < 3; i++) {
-        plot->addGraph();
+        plot->addGraph(rect1->axis(QCPAxis::atBottom), rect1->axis(QCPAxis::atLeft));
         plot->graph(i)->setLineStyle(QCPGraph::lsLine);
-        plot->graph(i)->setName(legend_name[i]);
+        plot->graph(i)->setName(name[i]);
     }
+
+    plot->addGraph(rect2->axis(QCPAxis::atBottom), rect2->axis(QCPAxis::atLeft));
 
     plot->graph(0)->setPen(QPen(QColor(255, 91, 0)));
     plot->graph(1)->setPen(QPen(QColor(92, 255, 54)));
     plot->graph(2)->setPen(QPen(QColor(60, 50, 255)));
+    plot->graph(3)->setPen(QPen(Qt::cyan));
 
-    plot->plotLayout()->insertRow(0);
-    QFont f("Consolas", 11, QFont::Bold);
-    plot->plotLayout()->addElement(0, 0, new QCPTextElement(plot, "Attitude", f));
-    plot->legend->setVisible(true);
-    plot->legend->setFont(font());
-    plot->xAxis->setLabel("time (s)");
-    plot->yAxis->setLabel("degree");
-    plot->xAxis->setRange(0, 10);
-    plot->yAxis->setRange(-10, 10);
-
-    plot->xAxis->grid();
-    plot->yAxis->grid();
-    plot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom);
-
-    plot = ui->customPlot_temperature;
-    plot->addGraph();
-    plot->graph(0)->setPen(QPen(Qt::cyan));
-    plot->plotLayout()->insertRow(0);
-    plot->plotLayout()->addElement(0, 0, new QCPTextElement(plot, "Temperature", f));
-    plot->legend->setFont(font());
-    plot->xAxis->setLabel("time (s)");
-    plot->yAxis->setLabel("degree Celsius");
-    plot->xAxis->setRange(0, 10);
-    plot->yAxis->setRange(25, 30);
-    plot->xAxis->grid();
-    plot->yAxis->grid();
-    plot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom);
+    plot->graph(0)->keyAxis()->setRange(0, 10);
+    plot->graph(3)->keyAxis()->setRange(0, 10);
+    plot->graph(3)->keyAxis()->setLabel("time (s)");
+    plot->graph(0)->valueAxis()->setRange(-10, 10);
+    plot->graph(3)->valueAxis()->setRange(25, 33);
+    plot->graph(0)->valueAxis()->setLabel("degree");
+    plot->graph(3)->valueAxis()->setLabel("degree Celsius");
 
     // Connect signal and slot
     connect(ui->comboBox_Port, SIGNAL(clicked()), this, SLOT(findAvaliablePort()));
@@ -137,16 +146,21 @@ void MainWindow::receiveDataSlot(QVector<double> data)
 {
     double x = (double)count / (double)(ui->spinBox_PacketRate->value());
 
-    for (int i = 0; i < data.size() - 1; i++) {
+    for (int i = 0; i < data.size(); i++) {
         ui->customPlot_attitude->graph(i)->addData(x, data.at(i));
     }
 
-    ui->customPlot_attitude->xAxis->setRange(x, 10, Qt::AlignRight);
-    ui->customPlot_attitude->replot();
+    QCustomPlot *plot = ui->customPlot_attitude;
+    plot->graph(0)->keyAxis()->setRange(x, 10, Qt::AlignRight);
+    plot->graph(3)->keyAxis()->setRange(x, 10, Qt::AlignRight);
+    plot->replot();
 
-    ui->customPlot_temperature->graph(0)->addData(x, data.at(3));
-    ui->customPlot_temperature->xAxis->setRange(x, 10, Qt::AlignRight);
-    ui->customPlot_temperature->replot();
+//    ui->customPlot_attitude->xAxis->setRange(x, 10, Qt::AlignRight);
+//    ui->customPlot_attitude->replot();
+
+//    ui->customPlot_temperature->graph(0)->addData(x, data.at(3));
+//    ui->customPlot_temperature->xAxis->setRange(x, 10, Qt::AlignRight);
+//    ui->customPlot_temperature->replot();
 
     get_data.append(data);
 
